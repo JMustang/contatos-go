bdbe770056ac047305e9b5d78e2e9cb608ac0618
Junior <eecfredes@gmail.com>
2025-06-25 21:34:59 -0300

    :rocket: Handler error

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,5 +1,11 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	errorhandler "github.com/JMustang/contatos-go/src/config/error_handler"
+	"github.com/gin-gonic/gin"
+)
 
-func CreateUser(c *gin.Context) {}
+func CreateUser(c *gin.Context) {
+	err := errorhandler.NewBadRequestError("❌ bad request")
+	c.JSON(err.Code, err)
+}
