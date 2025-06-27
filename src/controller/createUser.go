aad12c9ca62a95b0084b5e0dc9a7c9d4a4c1f9e4
Junior <eecfredes@gmail.com>
2025-06-26 22:03:40 -0300

    :rocket:User object

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,11 +1,26 @@
 package controller
 
 import (
+	"fmt"
+
 	errorhandler "github.com/JMustang/contatos-go/src/config/error_handler"
+	"github.com/JMustang/contatos-go/src/controller/model/request"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context) {
-	err := errorhandler.NewBadRequestError("❌ bad request")
-	c.JSON(err.Code, err)
+
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		rest_error := errorhandler.NewBadRequestError(
+			fmt.Sprintf("There are some incorrect fields, error=%s", err.Error()),
+		)
+
+		c.JSON(rest_error.Code, rest_error)
+		return
+	}
+
+	// TODO: Implementar lógica de criação do usuário
+	c.JSON(201, gin.H{"message": "User created successfully"})
 }
