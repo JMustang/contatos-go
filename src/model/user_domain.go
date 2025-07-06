51dbd794a3898a377b74d3d557152d8c2309d8b7
Junior <eecfredes@gmail.com>
2025-07-05 22:29:59 -0300

    :rocket:User domain interface

--- src/model/user_domain.go
+++ src/model/user_domain.go
@@ -1,9 +1,18 @@
 package model
 
-type userDomain struct {
+import errorhandler "github.com/JMustang/contatos-go/src/config/error_handler"
+
+type UserDomain struct {
 	id       string
 	email    string
 	password string
 	name     string
 	age      int8
+}
+
+type userDomainInterface interface {
+	CreateUser(UserDomain) *errorhandler.HandlerErr
+	UpdateUser(string, UserDomain) *errorhandler.HandlerErr
+	FindUser(string) (*UserDomain, *errorhandler.HandlerErr)
+	DeleteUser(string) *errorhandler.HandlerErr
 }
