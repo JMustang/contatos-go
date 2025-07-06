820fd2d5928f131b18bb9cd92ea24a1776a09a5a
Junior <eecfredes@gmail.com>
2025-07-05 22:30:29 -0300

    :rocket:Create user model

---
+++ src/model/create_user.go
@@ -0,0 +1,7 @@
+package model
+
+import errorhandler "github.com/JMustang/contatos-go/src/config/error_handler"
+
+func (*UserDomain) CreateUser(UserDomain) *errorhandler.HandlerErr {
+	return nil
+}
