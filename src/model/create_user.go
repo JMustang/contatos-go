f76acfc8ba3821e5647051dd80a47f7bcda09dff
Junior <eecfredes@gmail.com>
2025-07-07 21:57:24 -0300

    :rocket:encrypt create user

--- src/model/create_user.go
+++ src/model/create_user.go
@@ -1,7 +1,15 @@
 package model
 
-import errorhandler "github.com/JMustang/contatos-go/src/config/error_handler"
+import (
+	errorhandler "github.com/JMustang/contatos-go/src/config/error_handler"
+	"github.com/JMustang/contatos-go/src/config/logger"
+	"go.uber.org/zap"
+)
 
-func (*UserDomain) CreateUser(UserDomain) *errorhandler.HandlerErr {
+func (ud *UserDomain) CreateUser() *errorhandler.HandlerErr {
+
+	logger.Info("Init createUser model", zap.String("journey", "createUser"))
+	ud.EncryptPassword()
+
 	return nil
 }
