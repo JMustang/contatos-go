f6be2932d031bce8c777626320192435748c5014
Junior <eecfredes@gmail.com>
2025-07-07 21:56:41 -0300

    :rocket:Encrypt password

--- src/model/user_domain.go
+++ src/model/user_domain.go
@@ -1,18 +1,30 @@
 package model
 
-import errorhandler "github.com/JMustang/contatos-go/src/config/error_handler"
+import (
+	"crypto/md5"
+	"encoding/hex"
+
+	errorhandler "github.com/JMustang/contatos-go/src/config/error_handler"
+)
 
 type UserDomain struct {
-	id       string
-	email    string
-	password string
-	name     string
-	age      int8
+	// id       string
+	Email    string
+	Password string
+	Name     string
+	Age      int8
+}
+
+func (ud *UserDomain) EncryptPassword() {
+	hash := md5.New()
+	defer hash.Reset()
+	hash.Write([]byte(ud.Password))
+	ud.Password = hex.EncodeToString(hash.Sum(nil))
 }
 
 type userDomainInterface interface {
-	CreateUser(UserDomain) *errorhandler.HandlerErr
-	UpdateUser(string, UserDomain) *errorhandler.HandlerErr
+	CreateUser() *errorhandler.HandlerErr
+	UpdateUser(string) *errorhandler.HandlerErr
 	FindUser(string) (*UserDomain, *errorhandler.HandlerErr)
 	DeleteUser(string) *errorhandler.HandlerErr
 }
